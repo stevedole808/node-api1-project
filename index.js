6c39c362c13b5014af7c7607da6e2f59179cdeff
stevedole808 <stevedolelambda@gmail.com>
2020-01-13 13:50:18 -1000

    added get user by id and delete user by id

--- index.js
+++ index.js
@@ -21,24 +21,65 @@ server.get("/api/users", (req, res) => {
     .catch(error => {
       console.log(error);
       res.status(500).json({
-        errorMessage: "sorry, we ran into an error getting the list of users"
+        errorMessage: "he users information could not be retrieved."
       });
     });
 });
 
-server.post("/api/users", (req, res) => {
+server.post('/api/users', (req, res) => {
     const userData = req.body;
-
+    
     if (userData['name'] && userData['bio']) {
-         Users.insert(userData)
-           .then(user => {
-             res.status(201).json(user);
-           })
-           .catch(error => {
-             console.log(error);
-              res.status(500).json({ errorMessage: 'There was an error while saving the user to the database.' })
+        Users.insert(userData)
+            .then(user => {
+                res.status(201).json(user) 
+            })
+            .catch(error => {
+                res.status(500).json({ errorMessage: 'error creating user'})
             })
     } else {
         res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
     }
 })
+
+server.get("/api/users/:id", (req, res) => {
+    const id = req.params.id
+    if (id) {
+      Users.findById(id)
+        .then(users => {
+          res.status(200).json(users);
+        })
+        .catch(error => {
+          res.status(500).json({
+            errorMessage: "The user with the specified ID does not exist."
+          });
+        });
+    } else {
+      res
+        .status(404)
+        .json({
+          errorMessage: "The user with the specified ID does not exist."
+        });
+    }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    if(id) {
+        Users.remove(id)
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => {
+          res.status(500).json({
+            errorMessage: "error deleting user"
+          });
+        });
+    } else {
+      res
+        .status(404)
+        .json({
+          errorMessage: "The user with the specified ID does not exist."
+        });
+    }
+});
