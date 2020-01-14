5a41130229b9d2afdbc69b6377e5a2c153e09821
stevedole808 <stevedolelambda@gmail.com>
2020-01-14 09:42:03 -1000

    added put request

--- index.js
+++ index.js
@@ -83,3 +83,31 @@ server.delete('/api/users/:id', (req, res) => {
         });
     }
 });
+
+server.put("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id) {
+    if (userData["name"] && userData["bio"]) {
+      Users.update(id)
+        .then(updated => {
+          res.status(200).json(updated);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({
+              errorMessage: "The user information could not be modified."
+            });
+        });
+    } else {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    }
+  } else {
+    res
+      .status(404)
+      .json({ errorMessage: "The user with the specified ID does not exist." });
+  }
+});
